f8645075cfff9d69f83112a4d1b76ee3bc97d521
Josua Krause <joschi@accern.com>
2022-10-05 01:20:16 -0400

    indent

--- findpy.sh
+++ findpy.sh
@@ -1,13 +1,13 @@
 find . -type d \( \
-    -path './venv' -o \
-    -path './.*' -o \
-    -path './node_modules' -o \
-    -path './userdata' \
-    \) -prune -o \( \
-    -name '*.py' -o \
-    -name '*.pyi' \
-    \) \
-| grep -vF './venv' \
-| grep -vF './.' \
-| grep -vF './node_modules' \
-| grep -vF './userdata'
+        -path './venv' -o \
+        -path './.*' -o \
+        -path './node_modules' -o \
+        -path './userdata' \
+        \) -prune -o \( \
+        -name '*.py' -o \
+        -name '*.pyi' \
+        \) \
+    | grep -vF './venv' \
+    | grep -vF './.' \
+    | grep -vF './node_modules' \
+    | grep -vF './userdata'
