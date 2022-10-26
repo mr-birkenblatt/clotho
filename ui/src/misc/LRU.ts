c8daef82f896236268c9233e577eb1a9e5a09f34
Josua Krause <josua.krause@gmail.com>
2022-10-25 22:31:59 -0400

    better?

--- ui/src/misc/LRU.ts
+++ ui/src/misc/LRU.ts
@@ -1,163 +1,163 @@
-import { strict as assert } from "node:assert";
+import { strict as assert } from 'node:assert';
 
 class LRUEntry<K, V> {
-  parent: LRU<K, V>;
-  prev: LRUEntry<K, V>;
-  next: LRUEntry<K, V>;
-  key: K;
-  item: V;
+    parent: LRU<K, V>;
+    prev: LRUEntry<K, V>;
+    next: LRUEntry<K, V>;
+    key: K;
+    item: V;
 
-  constructor(parent: LRU<K, V>, key: K, item: V) {
-    this.parent = parent;
-    this.prev = this;
-    this.next = this;
-    this.key = key;
-    this.item = item;
-  }
+    constructor(parent: LRU<K, V>, key: K, item: V) {
+        this.parent = parent;
+        this.prev = this;
+        this.next = this;
+        this.key = key;
+        this.item = item;
+    }
 } // LRUEntry
 
 export default class LRU<K, V> {
-  head: LRUEntry<K, V> | undefined;
-  tail: LRUEntry<K, V> | undefined;
-  objs: Map<K, LRUEntry<K, V>>;
-  maxSize: number;
+    head: LRUEntry<K, V> | undefined;
+    tail: LRUEntry<K, V> | undefined;
+    objs: Map<K, LRUEntry<K, V>>;
+    maxSize: number;
 
-  constructor(maxSize: number) {
-    this.maxSize = Math.max(maxSize, 3);
-    this.head = undefined;
-    this.tail = undefined;
-    this.objs = new Map();
-  }
+    constructor(maxSize: number) {
+        this.maxSize = Math.max(maxSize, 3);
+        this.head = undefined;
+        this.tail = undefined;
+        this.objs = new Map();
+    }
 
-  set(key: K, value: V): V | undefined {
-    const entry = this.objs.get(key);
-    if (entry !== undefined) {
-      const res = entry.item;
-      entry.item = value;
-      return res;
+    set(key: K, value: V): V | undefined {
+        const entry = this.objs.get(key);
+        if (entry !== undefined) {
+            const res = entry.item;
+            entry.item = value;
+            return res;
+        }
+        const elem = new LRUEntry(this, key, value);
+        this.objs.set(key, elem);
+        const head = this.head;
+        this.head = elem;
+        if (head !== undefined) {
+            elem.next = head;
+            head.prev = elem;
+        } else {
+            assert.equal(this.tail, undefined);
+            this.tail = elem;
+        }
+        const tail = this.tail;
+        if (tail !== undefined && this.objs.size > this.maxSize) {
+            assert.equal(tail.next, tail);
+            assert.notEqual(tail.prev, tail);
+            this.tail = tail.prev;
+            tail.prev = tail;
+            assert.equal(this.tail.next, tail);
+            this.tail.next = this.tail;
+            this.objs.delete(tail.key);
+        }
+        return undefined;
     }
-    const elem = new LRUEntry(this, key, value);
-    this.objs.set(key, elem);
-    const head = this.head;
-    this.head = elem;
-    if (head !== undefined) {
-      elem.next = head;
-      head.prev = elem;
-    } else {
-      assert.equal(this.tail, undefined);
-      this.tail = elem;
-    }
-    const tail = this.tail;
-    if (tail !== undefined && this.objs.size > this.maxSize) {
-      assert.equal(tail.next, tail);
-      assert.notEqual(tail.prev, tail);
-      this.tail = tail.prev;
-      tail.prev = tail;
-      assert.equal(this.tail.next, tail);
-      this.tail.next = this.tail;
-      this.objs.delete(tail.key);
-    }
-    return undefined;
-  }
 
-  get(key: K): V | undefined {
-    const entry = this.objs.get(key);
-    if (entry === undefined) {
-      return undefined;
+    get(key: K): V | undefined {
+        const entry = this.objs.get(key);
+        if (entry === undefined) {
+            return undefined;
+        }
+        assert.ok(this.head !== undefined);
+        const head = this.head;
+        assert.equal(head.prev, head);
+        if (head !== entry) {
+            const prev = entry.prev;
+            assert.notEqual(prev, entry);
+            assert.equal(prev.next, entry);
+            const next = entry.next;
+            if (next === entry) {
+                assert.equal(this.tail, entry);
+                this.tail = prev;
+                prev.next = prev;
+            } else {
+                assert.equal(next.prev, entry);
+                prev.next = next;
+                next.prev = prev;
+            }
+            entry.prev = entry;
+            entry.next = head;
+            head.prev = entry;
+            this.head = entry;
+        }
+        return entry.item;
     }
-    assert.ok(this.head !== undefined);
-    const head = this.head;
-    assert.equal(head.prev, head);
-    if (head !== entry) {
-      const prev = entry.prev;
-      assert.notEqual(prev, entry);
-      assert.equal(prev.next, entry);
-      const next = entry.next;
-      if (next === entry) {
-        assert.equal(this.tail, entry);
-        this.tail = prev;
-        prev.next = prev;
-      } else {
-        assert.equal(next.prev, entry);
-        prev.next = next;
-        next.prev = prev;
-      }
-      entry.prev = entry;
-      entry.next = head;
-      head.prev = entry;
-      this.head = entry;
-    }
-    return entry.item;
-  }
 
-  has(key: K): boolean {
-    return this.objs.has(key);
-  }
+    has(key: K): boolean {
+        return this.objs.has(key);
+    }
 
-  delete(key: K): void {
-    const entry = this.objs.get(key);
-    if (entry === undefined) {
-      return;
+    delete(key: K): void {
+        const entry = this.objs.get(key);
+        if (entry === undefined) {
+            return;
+        }
+        if (this.head === entry && this.tail === entry) {
+            assert.equal(entry.prev, entry);
+            assert.equal(entry.next, entry);
+            this.head = undefined;
+            this.tail = undefined;
+        } else if (this.head === entry) {
+            assert.equal(entry.prev, entry);
+            assert.notEqual(entry.next, entry);
+            const next = entry.next;
+            assert.equal(next.prev, entry);
+            next.prev = next;
+            this.head = next;
+        } else if (this.tail === entry) {
+            assert.equal(entry.next, entry);
+            assert.notEqual(entry.prev, entry);
+            const prev = entry.prev;
+            assert.equal(prev.next, entry);
+            prev.next = prev;
+            this.tail = prev;
+        } else {
+            assert.notEqual(entry.next, entry);
+            assert.notEqual(entry.prev, entry);
+            const next = entry.next;
+            const prev = entry.prev;
+            prev.next = next;
+            next.prev = prev;
+            entry.next = entry;
+            entry.prev = entry;
+        }
+        this.objs.delete(key);
     }
-    if (this.head === entry && this.tail === entry) {
-      assert.equal(entry.prev, entry);
-      assert.equal(entry.next, entry);
-      this.head = undefined;
-      this.tail = undefined;
-    } else if (this.head === entry) {
-      assert.equal(entry.prev, entry);
-      assert.notEqual(entry.next, entry);
-      const next = entry.next;
-      assert.equal(next.prev, entry);
-      next.prev = next;
-      this.head = next;
-    } else if (this.tail === entry) {
-      assert.equal(entry.next, entry);
-      assert.notEqual(entry.prev, entry);
-      const prev = entry.prev;
-      assert.equal(prev.next, entry);
-      prev.next = prev;
-      this.tail = prev;
-    } else {
-      assert.notEqual(entry.next, entry);
-      assert.notEqual(entry.prev, entry);
-      const next = entry.next;
-      const prev = entry.prev;
-      prev.next = next;
-      next.prev = prev;
-      entry.next = entry;
-      entry.prev = entry;
-    }
-    this.objs.delete(key);
-  }
 
-  keys(): K[] {
-    let cur = this.head;
-    if (cur === undefined) {
-      return [];
+    keys(): K[] {
+        let cur = this.head;
+        if (cur === undefined) {
+            return [];
+        }
+        const arr: K[] = [];
+        while (cur.next !== cur) {
+            arr.push(cur.key);
+            cur = cur.next;
+            assert.ok(arr.length <= this.objs.size);
+        }
+        arr.push(cur.key);
+        return arr;
     }
-    const arr: K[] = [];
-    while (cur.next !== cur) {
-      arr.push(cur.key);
-      cur = cur.next;
-      assert.ok(arr.length <= this.objs.size);
-    }
-    arr.push(cur.key);
-    return arr;
-  }
 
-  values(): V[] {
-    let cur = this.head;
-    if (cur === undefined) {
-      return [];
+    values(): V[] {
+        let cur = this.head;
+        if (cur === undefined) {
+            return [];
+        }
+        const arr: V[] = [];
+        while (cur.next !== cur) {
+            arr.push(cur.item);
+            cur = cur.next;
+            assert.ok(arr.length <= this.objs.size);
+        }
+        arr.push(cur.item);
+        return arr;
     }
-    const arr: V[] = [];
-    while (cur.next !== cur) {
-      arr.push(cur.item);
-      cur = cur.next;
-      assert.ok(arr.length <= this.objs.size);
-    }
-    arr.push(cur.item);
-    return arr;
-  }
 } // LRU
--- ui/src/misc/genericLoader.ts
+++ ui/src/misc/genericLoader.ts
@@ -1,62 +1,67 @@
-import LRU from "./LRU.js";
+import LRU from './LRU.js';
 
 type ResultCB<V> = (arr: Map<number, V>) => void;
 type ContentCB<V> = (ready: boolean, content: V | undefined) => void;
 type ReadyCB = () => void;
 type LoadCB<V> = (
-  name: string,
-  offset: number,
-  size: number,
-  resultCb: ResultCB<V>
+    name: string,
+    offset: number,
+    size: number,
+    resultCb: ResultCB<V>
 ) => void;
 
 export default class GenericLoader<V> {
-  blockSize: number;
-  loadCb: LoadCB<V>;
-  lines: LRU<string, LRU<number, V>>;
-  activeLoads: Set<string>;
+    blockSize: number;
+    loadCb: LoadCB<V>;
+    lines: LRU<string, LRU<number, V>>;
+    activeLoads: Set<string>;
 
-  constructor(blockSize: number, loadCb: LoadCB<V>) {
-    this.blockSize = blockSize;
-    this.loadCb = loadCb;
-    this.lines = new LRU(10);
-    this.activeLoads = new Set<string>();
-  }
-
-  getLine(name: string): LRU<number, V> {
-    let res = this.lines.get(name);
-    if (res === undefined) {
-      res = new LRU<number, V>(100);
-      this.lines.set(name, res);
+    constructor(blockSize: number, loadCb: LoadCB<V>) {
+        this.blockSize = blockSize;
+        this.loadCb = loadCb;
+        this.lines = new LRU(10);
+        this.activeLoads = new Set<string>();
     }
-    return res;
-  }
 
-  get(name: string, index: number, contentCb: ContentCB<V>, readyCb: ReadyCB) {
-    const line = this.getLine(name);
-    const res = line.get(index);
-    if (res) {
-      return contentCb(true, res);
+    getLine(name: string): LRU<number, V> {
+        let res = this.lines.get(name);
+        if (res === undefined) {
+            res = new LRU<number, V>(100);
+            this.lines.set(name, res);
+        }
+        return res;
     }
-    const block = Math.floor(index / this.blockSize);
-    const blockName = `${name}-${block}`;
-    if (!this.activeLoads.has(blockName)) {
-      setTimeout(() => {
-        const offset = block * this.blockSize;
-        this.loadCb(name, offset, this.blockSize, (arr) => {
-          arr.forEach((v, ix) => {
-            line.set(ix, v);
-          });
-          this.activeLoads.delete(blockName);
-          readyCb();
-        });
-      }, 0);
-      this.activeLoads.add(blockName);
-    }
-    return contentCb(false, undefined);
-  }
 
-  unloadLine(name: string) {
-    this.lines.delete(name);
-  }
+    get(
+        name: string,
+        index: number,
+        contentCb: ContentCB<V>,
+        readyCb: ReadyCB
+    ) {
+        const line = this.getLine(name);
+        const res = line.get(index);
+        if (res) {
+            return contentCb(true, res);
+        }
+        const block = Math.floor(index / this.blockSize);
+        const blockName = `${name}-${block}`;
+        if (!this.activeLoads.has(blockName)) {
+            setTimeout(() => {
+                const offset = block * this.blockSize;
+                this.loadCb(name, offset, this.blockSize, (arr) => {
+                    arr.forEach((v, ix) => {
+                        line.set(ix, v);
+                    });
+                    this.activeLoads.delete(blockName);
+                    readyCb();
+                });
+            }, 0);
+            this.activeLoads.add(blockName);
+        }
+        return contentCb(false, undefined);
+    }
+
+    unloadLine(name: string) {
+        this.lines.delete(name);
+    }
 } // GenericLoader
