99a6e9fd988d53d5446346ee2cc6e4401a1e569c
Josua Krause <josua.krause@gmail.com>
2022-10-27 12:00:22 -0400

    move all files over

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,25 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+// import RequireLogin from './RequireLogin.js';
+import styled from 'styled-components';
+import ContentLoader from './contentLoader.js';
+import Horizontal from './Horizontal.js';
+import Vertical from './Vertical.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Main = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  background-color: #282c34;
+  color: white;
+`;
 
-export default App;
+// const MainHeader = styled.header`
+//   background-color: #282c34;
+//   min-height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   font-size: calc(10px + 2vmin);
+//   color: white;
+// `;
+
+const MainColumn = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+  width: 100%;
+  height: 100vh;
+  background-color: pink;
+  border: green 1px solid;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 0;
+  overflow: hidden;
+`;
+
+
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.loader = new ContentLoader();
+  }
+
+  getItem = (isParent, name, index, contentCb, readyCb) => {
+    return this.loader.getItem(isParent, name, index, contentCb, readyCb);
+  }
+
+  getVItem = (isParent, lineName, height) => {
+    return (
+      <Horizontal
+        itemWidth={450}
+        itemHeight={height}
+        itemRadius={10}
+        itemPadding={50}
+        buttonSize={50}
+        isParent={isParent}
+        lineName={lineName}
+        getItem={this.getItem} />
+    );
+  }
+
+  getChildLine = (lineName, cb) => {
+    this.loader.getChild(lineName, cb);
+  }
+
+  getParentLine = (lineName, cb) => {
+    this.loader.getParent(lineName, cb);
+  }
+
+  getLinkItems = (parentLineName, childLineName, parentIndex, childIndex) => {
+    return this.loader.getLinkInfo(
+      parentLineName,
+      childLineName,
+      parentIndex,
+      childIndex);
+  }
+
+  renderLinkItem = (link) => {
+    return (
+      <span>{link.key}: {link.count}</span>
+    );
+  }
+
+  render() {
+    return (
+      <Main>
+        {/* <MainHeader>
+          <RequireLogin />
+        </MainHeader> */}
+        <MainColumn>
+          <Vertical
+            getItem={this.getVItem}
+            getChildLine={this.getChildLine}
+            getParentLine={this.getParentLine}
+            getLinkItems={this.getLinkItems}
+            renderLinkItem={this.renderLinkItem}
+            height={450}
+            radius={10}
+            buttonSize={50} />
+        </MainColumn>
+      </Main>
+    );
+  }
+} // App
