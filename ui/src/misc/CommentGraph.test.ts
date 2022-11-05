518492b6c12549aba474e007d94b875e5578c7a3
Josua Krause <josua.krause@gmail.com>
2022-11-05 01:23:10 -0400

    make test harness generic

--- ui/src/misc/CommentGraph.test.ts
+++ ui/src/misc/CommentGraph.test.ts
@@ -2,6 +2,8 @@ import CommentGraph, {
   AdjustedLineIndex,
   FullKey,
   MHash,
+  NotifyContentCB,
+  NotifyLinkCB,
 } from './CommentGraph';
 import TestGraph from './TestGraph';
 import { assertTrue, range } from './util';
@@ -28,39 +30,38 @@ function asLinkKey(
   };
 }
 
-type Callback = (key: Readonly<MHash> | undefined, value: string) => void;
+type Callback<T extends any[]> = (...args: T) => void;
 
-async function execute(
-  expectWait: boolean,
-  pool: CommentGraph,
-  fun: (fullKey: Readonly<FullKey>, notify: Callback) => string | undefined,
-  fullKey: FullKey,
-  callback: Callback,
+async function execute<A extends any[], T extends any[], R>(
+  fun: (notify: Callback<T>, ...args: A) => R | undefined,
+  args: A,
+  callback: Callback<T>,
+  convertDirect: ((res: R) => T) | undefined,
 ): Promise<boolean> {
   const marker = jest.fn();
   return new Promise((resolve) => {
-    const cb: Callback = (key, value) => {
-      callback(key, value);
+    const cb: Callback<T> = (...cbArgs) => {
+      callback(...cbArgs);
       resolve(true);
     };
-    const notify: Callback = (key, value) => {
+    const notify: Callback<T> = (...cbArgs) => {
       marker();
-      if (expectWait) {
-        cb(key, value);
+      if (convertDirect === undefined) {
+        cb(...cbArgs);
       }
     };
-    const res = fun.call(pool, fullKey, notify);
+    const res = fun(notify, ...args);
     expect(marker).not.toBeCalled();
-    if (expectWait) {
+    if (convertDirect === undefined) {
       assertTrue(res === undefined);
     } else {
       assertTrue(res !== undefined);
-      cb(undefined, res);
+      cb(...convertDirect(res));
     }
     // console.log('runAllTimers');
     // jest.runAllTimers();
   }).then(() => {
-    if (expectWait) {
+    if (convertDirect === undefined) {
       expect(marker).toBeCalled();
       expect(marker).toHaveBeenCalledTimes(1);
     } else {
@@ -85,95 +86,93 @@ test('simple test comment graph', async () => {
   ]);
   graph.addTopics(['a', 'h']);
   const pool = new CommentGraph(graph.getApiProvider());
+
+  const getMessage = (notify: NotifyContentCB, fullKey: FullKey): string | undefined => {
+    return pool.getMessage(fullKey, notify);
+  };
+  const convertMessage = (res: string): [undefined, string] => {
+    return [undefined, res];
+  };
   await execute(
-    true,
-    pool,
-    pool.getMessage,
-    asTopicKey(0),
+    getMessage,
+    [asTopicKey(0)],
     (mhash, content) => {
       expect(mhash).toEqual('a');
       expect(content).toEqual('msg: a');
     },
+    undefined,
   );
   await execute(
-    false,
-    pool,
-    pool.getMessage,
-    asTopicKey(1),
+    getMessage,
+    [asTopicKey(1)],
     (mhash, content) => {
       expect(mhash).toBe(undefined);
       expect(content).toEqual('msg: h');
     },
+    convertMessage,
   );
   await execute(
-    false,
-    pool,
-    pool.getMessage,
-    asTopicKey(-1),
+    getMessage,
+    [asTopicKey(-1)],
     (mhash, content) => {
       expect(mhash).toBe(undefined);
       expect(content).toEqual('[unavailable]');
     },
+    convertMessage,
   );
   await execute(
-    false,
-    pool,
-    pool.getMessage,
-    asTopicKey(2),
+    getMessage,
+    [asTopicKey(2)],
     (mhash, content) => {
       expect(mhash).toBe(undefined);
       expect(content).toEqual('[unavailable]');
     },
+    convertMessage,
   );
   await execute(
-    true,
-    pool,
-    pool.getMessage,
-    asLinkKey('a', false, 0),
+    getMessage,
+    [asLinkKey('a', false, 0)],
     (mhash, content) => {
       expect(mhash).toEqual('b');
       expect(content).toEqual('msg: b');
     },
+    undefined,
   );
   await execute(
-    true,
-    pool,
-    pool.getMessage,
-    asLinkKey('a', false, 2),
+    getMessage,
+    [asLinkKey('a', false, 2)],
     (mhash, content) => {
       expect(mhash).toEqual('d');
       expect(content).toEqual('msg: d');
     },
+    undefined,
   );
   await execute(
-    false,
-    pool,
-    pool.getMessage,
-    asLinkKey('a', false, 2),
+    getMessage,
+    [asLinkKey('a', false, 2)],
     (mhash, content) => {
       expect(mhash).toBe(undefined);
       expect(content).toEqual('msg: d');
     },
+    convertMessage,
   );
   await execute(
-    true,
-    pool,
-    pool.getMessage,
-    asLinkKey('a', false, 4),
+    getMessage,
+    [asLinkKey('a', false, 4)],
     (mhash, content) => {
       expect(mhash).toEqual('f');
       expect(content).toEqual('msg: f');
     },
+    undefined,
   );
   await execute(
-    false,
-    pool,
-    pool.getMessage,
-    asLinkKey('a', false, 5),
+    getMessage,
+    [asLinkKey('a', false, 5)],
     (mhash, content) => {
       expect(mhash).toBe(undefined);
       expect(content).toEqual('[deleted]');
     },
+    convertMessage,
   );
   const hashes = ['b', 'c', 'd', 'e', 'f'];
   const contents = hashes.map((el) => `msg: ${el}`);
@@ -181,59 +180,54 @@ test('simple test comment graph', async () => {
   await Promise.all(
     range(5).map((ix) => {
       return execute(
-        true,
-        pool,
-        pool.getMessage,
-        asLinkKey('a', false, ix),
+        getMessage,
+        [asLinkKey('a', false, ix)],
         (mhash, content) => {
           expect(mhash).toEqual(hashes[ix]);
           expect(content).toEqual(contents[ix]);
         },
+        undefined,
       );
     }),
   );
   await Promise.all(
     range(5).map((ix) => {
       return execute(
-        false,
-        pool,
-        pool.getMessage,
-        asLinkKey('a', false, ix),
+        getMessage,
+        [asLinkKey('a', false, ix)],
         (mhash, content) => {
           expect(mhash).toBe(undefined);
           expect(content).toEqual(contents[ix]);
         },
+        convertMessage,
       );
     }),
   );
   await execute(
-    true,
-    pool,
-    pool.getMessage,
-    asLinkKey('b', true, 0),
+    getMessage,
+    [asLinkKey('b', true, 0)],
     (mhash, content) => {
       expect(mhash).toBe('a');
       expect(content).toEqual('msg: a');
     },
+    undefined,
   );
   await execute(
-    true,
-    pool,
-    pool.getMessage,
-    asLinkKey('b', true, 1),
+    getMessage,
+    [asLinkKey('b', true, 1)],
     (mhash, content) => {
       expect(mhash).toBe('g');
       expect(content).toEqual('msg: g');
     },
+    undefined,
   );
   await execute(
-    false,
-    pool,
-    pool.getMessage,
-    asLinkKey('b', true, 2),
+    getMessage,
+    [asLinkKey('b', true, 2)],
     (mhash, content) => {
       expect(mhash).toBe(undefined);
       expect(content).toEqual('[deleted]');
     },
+    convertMessage,
   );
 });
