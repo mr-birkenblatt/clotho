20e0b3dd3c992be12ea5c6cdb8c7da6000c728da
Josua Krause <josua.krause@gmail.com>
2022-12-15 18:45:22 -0500

    fetch more

--- ui/src/misc/constants.ts
+++ ui/src/misc/constants.ts
@@ -1,5 +1,5 @@
 export const URL_PREFIX = `${window.location.origin}/api`;
-export const DEFAULT_BLOCK_SIZE = 5;
+export const DEFAULT_BLOCK_SIZE = 10;
 export const DEFAULT_COMMENT_POOL_SIZE = 10000;
 export const DEFAULT_LINE_SIZE = 100;
 export const DEFAULT_LINK_POOL_SIZE = 1000;
